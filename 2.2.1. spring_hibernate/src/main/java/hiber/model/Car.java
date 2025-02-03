67494aa8731b02cdc104634230b56902ec90e592
Konstantin <kon.sharanin@gmail.com>
2025-02-03 14:09:02 +0300

    add getter, setter and toString() to User and Car classes

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -20,6 +20,38 @@ public class Car {
     @OneToOne(mappedBy = "car")
     private User user;
 
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
     public User getUser() {
         return user;
     }
@@ -28,11 +60,12 @@ public class Car {
         this.user = user;
     }
 
-    public Car() {
-    }
-
-    public Car(String model, int series) {
-        this.model = model;
-        this.series = series;
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                ", user=" + user +
+                '}';
     }
 }
